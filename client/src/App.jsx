4439bf381219a23a9fa17f693ca7a7200985832b
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-08-09 12:03:38 +0530

    files added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,47 @@
-import "./App.css";
+import "./App.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
+import Products from "./pages/products/Products";
+import Product from "./pages/product/Product";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
